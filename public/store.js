c97387fb25a0b27417974ece9dcac69e9c6ed3a5
antonm <antonm@ca.ibm.com>
2016-06-07 00:53:38 -0400

    First cut

---
+++ public/store.js
@@ -0,0 +1,57 @@
+ function get(path, callback) {
+     var xmlhttp = new XMLHttpRequest();
+     xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+             callback(JSON.parse(xmlhttp.responseText));
+         }
+     }
+     xmlhttp.open("GET", path, true);
+     xmlhttp.send();
+ }
+
+
+ function buildItem(item) {
+
+     var html = '';
+
+     html = html + '<div class="info">' +
+         '<div class="meta">' +
+         '<div class="name">' + item.name + '</div>' +
+         '<div class="description">' + item.description + '</div>' +
+         '<div class="cost">$' + item.usaDollarPrice + '</div>' +
+         '</div>' +
+         '<div class="style"></div>' +
+         '</div>' +
+         '<div class="product">' +
+         '<img class="pic" src="' + item.imgsrc + '"></div>'
+
+     return html;
+ }
+
+ function setup() {
+
+     console.log('setup');
+
+
+
+     get('./data/sample.json', function (items) {
+
+         var anchor = document.getElementById('items');
+
+         items.forEach(function (item) {
+             console.log(item);
+
+             var product = document.createElement('div');
+             product.className = 'tile';
+             product.innerHTML = buildItem(item);
+
+             anchor.appendChild(product);
+
+
+
+         });
+     })
+ }
+
+
+ window.onload = setup;
