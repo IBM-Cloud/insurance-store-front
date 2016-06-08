18a8e35529a63a272d279389cc7310ac4996d933
antonm <antonm@ca.ibm.com>
2016-06-08 00:02:07 -0400

    Cloud Co evolution

--- public/store.js
+++ public/store.js
@@ -1,45 +1,11 @@
- function get(path, callback) {
-     var xmlhttp = new XMLHttpRequest();
-     xmlhttp.onreadystatechange = function () {
-         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-             callback(JSON.parse(xmlhttp.responseText));
-         }
-     }
-     xmlhttp.open("GET", path, true);
-     xmlhttp.send();
- }
+function openTravel() {
+    console.log('click');
 
- function buildItem(item) {
+    window.location = "travel.html"
+}
 
-     var html = '';
+function openWatson() {
+    console.log('click');
 
-     html = html + '<div class="info">' +
-         '<div class="meta">' +
-         '<div class="name">' + item.name + '</div>' +
-         '<div class="description">' + item.description + '</div>' +
-         '<div class="cost">$' + item.usaDollarPrice + '</div>' +
-         '</div>' +
-         '<div class="style"></div>' +
-         '</div>' +
-         '<div class="product">' +
-         '<img class="pic" src="' + item.imgsrc + '"></div>'
-
-     return html;
- }
-
- function setup() {
-
-     get('./data/sample.json', function (items) {
-
-         var anchor = document.getElementById('items');
-
-         items.forEach(function (item) {
-             var product = document.createElement('div');
-             product.className = 'tile';
-             product.innerHTML = buildItem(item);
-             anchor.appendChild(product);
-         });
-     })
- }
-
- window.onload = setup;
+    window.location = "watson.html"
+}
