48e33200941b6a6ee6cf3d74a7c166c57c02369c
antonm <antonm@ca.ibm.com>
2016-06-08 19:47:18 -0400

    committing next steps - more to come

--- public/src/watson.js
+++ public/src/watson.js
@@ -5,23 +5,35 @@ var PolicyBuilder = function () {
 PolicyBuilder.prototype.selectedCriteria = [];
 
 PolicyBuilder.prototype.criteria = [{
-    "label": "Trip Duration",
-    "image": "calendar.svg"
+    "label": "Duration",
+    "image": "calendar.svg",
+    "min": 1,
+    "max": 5
     }, {
-    "label": "Recommendation",
-    "image": "recommendation.svg"
+    "label": "Reviews",
+    "image": "recommendation.svg",
+    "min": 1,
+    "max": 5
     }, {
     "label": "Cost",
-    "image": "cost.svg"
+    "image": "cost.svg",
+    "min": 1,
+    "max": 5
     }, {
     "label": "Coverage",
-    "image": "guage.svg"
+    "image": "guage.svg",
+    "min": 1,
+    "max": 5
     }, {
-    "label": "Care",
-    "image": "health.svg"
+    "label": "Quality",
+    "image": "health.svg",
+    "min": 1,
+    "max": 5
     }, {
-    "label": "Value of Trip",
-    "image": "world.svg"
+    "label": "Value",
+    "image": "world.svg",
+    "min": 1,
+    "max": 5
     }]
 
 PolicyBuilder.prototype.selectCriteria = function (label) {
@@ -52,11 +64,14 @@ PolicyBuilder.prototype.selectCriteria = function (label) {
     this.selectedCriteria = newSelections;
 
     var processButton = document.getElementById('process');
+    var instructions = document.getElementById('instructions');
 
     if (this.selectedCriteria.length > 0) {
         processButton.disabled = false;
+        instructions.className = 'disabledInstructions';
     } else {
         processButton.disabled = true;
+        instructions.className = 'instructions';
     }
 }
 
@@ -66,7 +81,7 @@ PolicyBuilder.prototype.makeCritria = function (label, image) {
     element.className = 'criteria';
     element.id = label;
     element.innerHTML = '<img src = "images/wash/' + image + '" class = "criteria-image">' +
-        '<label class = "criteria-label">' + label + '</label>';
+        '<label class = "label">' + label + '</label>';
 
     var pb = this;
     element.onclick = function () {
@@ -90,8 +105,47 @@ PolicyBuilder.prototype.addCriteria = function () {
     processButton.disabled = true;
 }
 
+
+PolicyBuilder.prototype.makeEvaluation = function (criteria) {
+
+    var evaluation = document.createElement('div');
+    evaluation.className = 'evaluation';
+    evaluation.innerHTML =
+
+        '<img src = "images/wash/' + criteria.image + '" class = "criteria-image">' +
+        '<label class = "criteria-label">' + criteria.label + '</label>' +
+        '<label class = "slider-label" >' + criteria.min + '</label >' +
+        '<input class = "slider" id = "' + criteria.name + '" type = "range" min = "' + criteria.min + '"max = "' + criteria.max + '"step = "0.1"/>' +
+        '<label class = "slider-label">' + criteria.max + '</label>';
+
+    return evaluation;
+}
+
 PolicyBuilder.prototype.process = function () {
 
+    var input = document.getElementById('elements');
+    input.style.display = 'none';
+
+    var button = document.getElementById('process');
+    button.style.display = 'none';
+
+    var filter = document.getElementById('filter');
+
+    var pb = this;
+
+    pb.selectedCriteria.forEach(function (criteria) {
+
+        pb.criteria.forEach(function (data) {
+
+            if (data.label === criteria) {
+
+                var evaluation = pb.makeEvaluation(data);
+                filter.appendChild(evaluation);
+            }
+        });
+    })
+    var output = document.getElementById('output');
+    output.style.display = 'inherit';
 
 }
 
